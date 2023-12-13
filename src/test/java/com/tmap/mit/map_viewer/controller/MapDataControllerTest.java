9e04848efb355e0429d119bf07ed32448e1cb773
T1100492_tmobi <T1100492@tmapmobility.com>
2023-12-13 11:31:53 +0900

    VSM-1252 shape file parser 백엔드 진행 (arc, polygon)

--- src/test/java/com/tmap/mit/map_viewer/controller/MapDataControllerTest.java
+++ src/test/java/com/tmap/mit/map_viewer/controller/MapDataControllerTest.java
@@ -2,13 +2,15 @@ package com.tmap.mit.map_viewer.controller;
 
 import com.tmap.mit.map_viewer.dto.ShapeData;
 import com.tmap.mit.map_viewer.service.MapDataService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
+
+import java.io.IOException;
 
 import static org.mockito.Mockito.when;
 
@@ -21,16 +23,20 @@ class MapDataControllerTest {
     @MockBean
     private MapDataService mapDataService;
 
+    private final String GET_MAP_DATA_URI_FORMMAT = "/mapData/%s";
     private final String POINT_FILE_NAME = "Duraklar";
-    private final String URI_PATH_FORMAT = "/mapData/%s";
 
     @Test
-    void createByShapeFile(){
-        when(mapDataService.createByShapeFile(POINT_FILE_NAME)).thenReturn(
-                Flux.just(ShapeData.builder().build())
-        );
+    @DisplayName("shape file에서 map data를 추출하는 api 컨트롤러 테스트")
+    void givenFileName_whenGetMapDataByShapeFile_thenResponseCheck() throws IOException {
+        // given
+        String fileName = POINT_FILE_NAME;
 
-        webTestClient.get().uri(String.format(URI_PATH_FORMAT, POINT_FILE_NAME))
+        // when & then
+        ShapeData mapData = mapDataService.getMapDataByShapeFile(fileName);
+        when(mapDataService.getMapDataByShapeFile(POINT_FILE_NAME)).thenReturn(mapData);
+
+        webTestClient.get().uri(String.format(GET_MAP_DATA_URI_FORMMAT, POINT_FILE_NAME))
                 .exchange()
                 .expectStatus().is2xxSuccessful();
     }
