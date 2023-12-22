93332a8b7eb70f67d2b85762551480e4b75589b9
cgyi29 <cgyi29@tmobility.com>
2023-12-22 09:49:47 +0900

    VSM-1268 화면 randering 과정 중 api 통신 비용이 차지하는 비율을 줄이고자 Caffein Cache 적용

---
+++ src/main/java/com/tmap/mit/map_viewer/config/cache/CaffeineCacheConfig.java
@@ -0,0 +1,38 @@
+package com.tmap.mit.map_viewer.config.cache;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.tmap.mit.map_viewer.cd.CaffeineCacheType;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * caffein cache 적용
+ * inmemory cache - simpleCache
+ */
+
+@Configuration
+public class CaffeineCacheConfig {
+    @Bean
+    public CacheManager caffeineConfig() {
+        List<CaffeineCache> caches = Arrays.stream(CaffeineCacheType.values())
+                .map(cache -> new CaffeineCache(
+                        cache.getCacheName(),
+                        Caffeine.newBuilder()
+                                .expireAfterWrite(cache.getExpiredAfterWrite(), cache.getTimeUtint())
+                                .maximumSize(cache.getMaximumSize())
+                                .recordStats()
+                                .build()
+                )).toList();
+
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(caches);
+
+        return cacheManager;
+    }
+}
